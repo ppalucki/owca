50b20babf96ecf843ebc5477579836d48113a5f8
Pawel Palucki <ppalucki@users.noreply.github.com>
2019-01-17 11:58:59 +0100

    allocation: Allocators module (#26)
    * Allocators module.
      Includes all the necessary components for enable allocation:
      - base module and no-op implementation
      - encoding metrics for allocations
      - required function to operator on RDTAllocation objects
    * logger trace to support verbose mode
    * Serializable and Mergeable base classes

--- owca/allocators.py
+++ owca/allocators.py
@@ -11,16 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
 from enum import Enum
-from typing import List, Dict, Union
+from pprint import pformat
+from typing import List, Dict, Union, Tuple, Optional
+import logging
 
-from owca.metrics import Metric
+from owca.logger import trace
+from owca.metrics import Metric, MetricType
 from owca.mesos import TaskId
 from owca.platforms import Platform
 from owca.detectors import TasksMeasurements, TasksResources, TasksLabels, Anomaly
+
+from dataclasses import dataclass
+
+log = logging.getLogger(__name__)
 
 
 class AllocationType(str, Enum):
@@ -30,14 +35,102 @@ class AllocationType(str, Enum):
     RDT = 'rdt'
 
 
-@dataclass
-class RDTAllocation:
+class MergeableAllocationValue(ABC):
+
+    @abstractmethod
+    def merge_with_current(self, current: 'MergeableAllocationValue') -> Tuple[
+            'MergeableAllocationValue', 'MergeableAllocationValue']:
+        ...
+
+
+class SerializableAllocationValue(ABC):
+
+    @abstractmethod
+    def generate_metrics(self) -> List[Metric]:
+        ...
+
+
+@dataclass(unsafe_hash=True)
+class RDTAllocation(SerializableAllocationValue, MergeableAllocationValue):
     # defaults to TaskId from TasksAllocations
     name: str = None
     # CAT: optional - when no provided doesn't change the existing allocation
     l3: str = None
     # MBM: optional - when no provided doesn't change the existing allocation
     mb: str = None
+
+    def generate_metrics(self) -> List[Metric]:
+        """Encode RDT Allocation as metrics.
+        Note:
+        - cache allocation: generated two metrics, with number of cache ways and
+                            mask of bits (encoded as int)
+        - memory bandwidth: is encoded as int, representing MB/s or percentage
+        """
+        # Empty object generate no metric.
+        if not self.l3 and not self.mb:
+            return []
+
+        group_name = self.name or ''
+
+        metrics = []
+        if self.l3:
+            domains = _parse_schemata_file_row(self.l3)
+            for domain_id, raw_value in domains.items():
+                metrics.extend([
+                    Metric(
+                        name='allocation', value=_count_enabled_bits(raw_value),
+                        type=MetricType.GAUGE, labels=dict(
+                            allocation_type='rdt_l3_cache_ways', group_name=group_name,
+                            domain_id=domain_id)
+                    ),
+                    Metric(
+                        name='allocation', value=int(raw_value, 16),
+                        type=MetricType.GAUGE, labels=dict(
+                            allocation_type='rdt_l3_mask', group_name=group_name,
+                            domain_id=domain_id)
+                    )
+                ])
+
+        if self.mb:
+            domains = _parse_schemata_file_row(self.mb)
+            for domain_id, raw_value in domains.items():
+                # NOTE: raw_value is treated as int, ignoring unit used (MB or %)
+                value = int(raw_value)
+                metrics.append(
+                    Metric(
+                       name='allocation', value=value, type=MetricType.GAUGE,
+                       labels=dict(allocation_type='rdt_mb',
+                                   group_name=group_name, domain_id=domain_id)
+                    )
+                )
+
+        return metrics
+
+    @trace(log, verbose=True)
+    def merge_with_current(self: Optional['RDTAllocation'],
+                           current_rdt_allocation: 'RDTAllocation') -> \
+            Tuple['RDTAllocation', 'RDTAllocation']:
+        """Merge with existing RDTAllocation objects and return
+        sum of the allocations (target_rdt_allocation) and allocations that need to be updated
+        (rdt_allocation_changeset)."""
+        new_rdt_allocation = self
+        # new name, then new allocation will be used (overwrite) but no merge
+        if current_rdt_allocation is None or current_rdt_allocation.name != new_rdt_allocation.name:
+            return new_rdt_allocation, new_rdt_allocation
+        else:
+            target_rdt_allocation = RDTAllocation(
+                name=current_rdt_allocation.name,
+                l3=new_rdt_allocation.l3 or current_rdt_allocation.l3,
+                mb=new_rdt_allocation.mb or current_rdt_allocation.mb,
+            )
+            rdt_allocation_changeset = RDTAllocation(
+                name=new_rdt_allocation.name,
+            )
+            if current_rdt_allocation.l3 != new_rdt_allocation.l3:
+                rdt_allocation_changeset.l3 = new_rdt_allocation.l3
+            if current_rdt_allocation.mb != new_rdt_allocation.mb:
+                rdt_allocation_changeset.mb = new_rdt_allocation.mb
+            return target_rdt_allocation, rdt_allocation_changeset
 
 
 TaskAllocations = Dict[AllocationType, Union[float, RDTAllocation]]
@@ -48,12 +141,17 @@ TasksAllocations = Dict[TaskId, TaskAllocations]
 class AllocationConfiguration:
 
     # Default value for cpu.cpu_period [ms] (used as denominator).
-    cpu_quota_period: int = 100
+    cpu_quota_period: int = 1000
 
     # Number of minimum shares, when ``cpu_shares`` allocation is set to 0.0.
     cpu_shares_min: int = 2
     # Number of shares to set, when ``cpu_shares`` allocation is set to 1.0.
     cpu_shares_max: int = 10000
+
+    # Default Allocation for default root group during initilization.
+    # It will be used as default for all tasks (None will set to maximum available value).
+    default_rdt_l3: str = None
+    default_rdt_mb: str = None
 
 
 class Allocator(ABC):
@@ -73,10 +171,181 @@ class Allocator(ABC):
 class NOPAllocator(Allocator):
 
     def allocate(self, platform, tasks_measurements, tasks_resources,
-                 tasks_labels, task_allocations):
+                 tasks_labels, tasks_allocations):
         return [], [], []
 
 
-def convert_allocations_to_metrics(allocations) -> List[Metric]:
-    # TODO: convenrt allocations to metrics
-    return []
+# -----------------------------------------------------------------------
+# private logic to handle allocations
+# -----------------------------------------------------------------------
+
+def _convert_tasks_allocations_to_metrics(tasks_allocations: TasksAllocations) -> List[Metric]:
+    """Takes allocations on input and convert them to something that can be
+    stored persistently as metrics adding type fields and labels.
+
+    Simple allocations become simple metric like this:
+    - Metric(name='allocation', type='cpu_shares', value=0.2, labels=(task_id='some_task_id'))
+    Encoding RDTAllocation object is delegated to RDTAllocation class.
+    """
+    metrics = []
+    for task_id, task_allocations in tasks_allocations.items():
+        task_allocations: TaskAllocations
+        for allocation_type, allocation_value in task_allocations.items():
+
+            if isinstance(allocation_value, SerializableAllocationValue):
+                this_allocation_metrics = allocation_value.generate_metrics()
+                for metric in this_allocation_metrics:
+                    metric.labels.update(task_id=task_id)
+                metrics.extend(this_allocation_metrics)
+
+            elif isinstance(allocation_value, (float, int)):
+                # Default metrics encoding method for float and integers values.
+                # Simple encoding for
+                metrics.append(
+                    Metric(name='allocation', value=allocation_value, type=MetricType.GAUGE,
+                           labels=dict(allocation_type=allocation_type, task_id=task_id)
+                           )
+                )
+            else:
+                raise NotImplementedError(
+                    'encoding AllocationType=%r for value of type=%r is '
+                    'not supported!' % (allocation_type, type(allocation_value))
+                )
+
+    return metrics
+
+
+@trace(log, verbose=False)
+def _calculate_task_allocations_changeset(
+        current_task_allocations: TaskAllocations,
+        new_task_allocations: TaskAllocations)\
+        -> Tuple[TaskAllocations, TaskAllocations]:
+    """Return tuple of resource allocation (changeset) per task.
+    """
+    target_task_allocations: TaskAllocations = dict(current_task_allocations)
+    task_allocations_changeset: TaskAllocations = {}
+
+    for allocation_type, new_allocation_value in new_task_allocations.items():
+        if isinstance(new_allocation_value, MergeableAllocationValue):
+            current_rdt_allocations = current_task_allocations.get(AllocationType.RDT)
+            target_rdt_allocation, rdt_allocation_changeset = \
+                new_allocation_value.merge_with_current(current_rdt_allocations)
+            target_rdt_allocation: RDTAllocation
+            rdt_allocation_changeset: RDTAllocation
+            target_task_allocations[AllocationType.RDT] = target_rdt_allocation
+            if rdt_allocation_changeset.l3 is not None or rdt_allocation_changeset.mb is not None:
+                task_allocations_changeset[AllocationType.RDT] = rdt_allocation_changeset
+
+        else:
+            if allocation_type not in target_task_allocations or \
+                    target_task_allocations[allocation_type] != new_allocation_value:
+                target_task_allocations[allocation_type] = new_allocation_value
+                task_allocations_changeset[allocation_type] = new_allocation_value
+
+    if task_allocations_changeset:
+        log.debug('_calculate_task_allocations_changeset():' +
+                  '\ncurrent_task_allocations=\n%s\nnew_task_allocations=\n%s',
+                  pformat(current_task_allocations), pformat(new_task_allocations))
+        log.debug('_calculate_task_allocations_changeset():\ntarget_task_allocations=\n%s' +
+                  '\ntask_allocations_changeset=\n%s',
+                  pformat(target_task_allocations), pformat(task_allocations_changeset))
+    return target_task_allocations, task_allocations_changeset
+
+
+def _calculate_tasks_allocations_changeset(
+        current_tasks_allocations: TasksAllocations, new_tasks_allocations: TasksAllocations) \
+        -> Tuple[TasksAllocations, TasksAllocations]:
+    """Return tasks allocations that need to be applied.
+    Takes as input:
+    1) current_tasks_allocations: currently applied allocations in the system,
+    2) new_tasks_allocations: new to be applied allocations
+
+    and outputs:
+    1) target_tasks_allocations: the list of all allocations which will
+        be applied in the system in next step:
+       so it is a sum of inputs (if the are conflicting allocations
+       in both inputs the value is taken from new_tasks_allocations).
+    2) tasks_allocations_changeset: only allocations from
+       new_tasks_allocations which are not contained already
+       in current_tasks_allocations (set difference
+       of input new_tasks_allocations and input current_tasks_allocations)
+    """
+    target_tasks_allocations: TasksAllocations = {}
+    tasks_allocations_changeset: TasksAllocations = {}
+
+    # check and merge & overwrite with old allocations
+    for task_id, current_task_allocations in current_tasks_allocations.items():
+        if task_id in new_tasks_allocations:
+            new_task_allocations = new_tasks_allocations[task_id]
+            target_task_allocations, changeset_task_allocations = \
+                _calculate_task_allocations_changeset(current_task_allocations,
+                                                      new_task_allocations)
+            target_tasks_allocations[task_id] = target_task_allocations
+            tasks_allocations_changeset[task_id] = changeset_task_allocations
+        else:
+            target_tasks_allocations[task_id] = current_task_allocations
+
+    # if there are any new_allocations on task level that yet not exists in old_allocations
+    # then just add them to both list
+    only_new_tasks_ids = set(new_tasks_allocations) - set(current_tasks_allocations)
+    for only_new_task_id in only_new_tasks_ids:
+        task_allocations = new_tasks_allocations[only_new_task_id]
+        target_tasks_allocations[only_new_task_id] = task_allocations
+        tasks_allocations_changeset[only_new_task_id] = task_allocations
+
+    return target_tasks_allocations, tasks_allocations_changeset
+
+
+def _parse_schemata_file_row(line: str) -> Dict[str, str]:
+    """Parse RDTAllocation.l3 and RDTAllocation.mb strings based on
+    https://elixir.bootlin.com/linux/latest/source/arch/x86/kernel/cpu/intel_rdt_ctrlmondata.c#lL206
+    and return dict mapping and domain id to its configuration (value).
+    Resource type (e.g. mb, l3) is dropped.
+
+    Eg.
+    mb:1=20;2=50 returns {'1':'20', '2':'50'}
+    mb:xxx=20mbs;2=50b returns {'1':'20mbs', '2':'50b'}
+    raises ValueError exception for inproper format or conflicting domains ids.
+    """
+    RESOURCE_ID_SEPARATOR = ':'
+    DOMAIN_ID_SEPARATOR = ';'
+    VALUE_SEPARATOR = '='
+
+    domains = {}
+
+    # Ignore emtpy line.
+    if not line:
+        return {}
+
+    # Drop resource identifier prefix like ("mb:")
+    line = line[line.find(RESOURCE_ID_SEPARATOR)+1:]
+    # Domains
+    domains_with_values = line.split(DOMAIN_ID_SEPARATOR)
+    for domain_with_value in domains_with_values:
+        if not domain_with_value:
+            raise ValueError('domain cannot be empty')
+        if VALUE_SEPARATOR not in domain_with_value:
+            raise ValueError('Value separator is missing "="!')
+        separator_position = domain_with_value.find(VALUE_SEPARATOR)
+        domain_id = domain_with_value[:separator_position]
+        if not domain_id:
+            raise ValueError('domain_id cannot be empty!')
+        value = domain_with_value[separator_position+1:]
+        if not value:
+            raise ValueError('value cannot be empty!')
+
+        if domain_id in domains:
+            raise ValueError('Conflicting domain id found!')
+
+        domains[domain_id] = value
+
+    return domains
+
+
+def _count_enabled_bits(hexstr: str) -> int:
+    """Parse a raw value like f202 to number of bits enabled."""
+    if hexstr == '':
+        return 0
+    value_int = int(hexstr, 16)
+    enabled_bits_count = bin(value_int).count('1')
+    return enabled_bits_count
---
+++ tests/test_allocations.py
@@ -0,0 +1,236 @@
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from owca.allocators import _calculate_task_allocations_changeset, \
+    _calculate_tasks_allocations_changeset, RDTAllocation, AllocationType, \
+    _convert_tasks_allocations_to_metrics, _parse_schemata_file_row, \
+    _count_enabled_bits
+from owca.metrics import Metric, MetricType
+
+
+@pytest.mark.parametrize(
+    'current_task_allocations,new_task_allocations,'
+    'expected_target_task_allocations,expected_task_allocations_changeset', (
+     ({}, {},
+      {}, {}),
+     ({'a': 0.2}, {},
+      {'a': 0.2}, {}),
+     ({'a': 0.2}, {'a': 0.2},
+      {'a': 0.2}, {}),
+     ({'b': 2}, {'b': 3},
+      {'b': 3}, {'b': 3}),
+     ({'a': 0.2, 'b': 0.4}, {'a': 0.2, 'b': 0.5},
+      {'a': 0.2, 'b': 0.5}, {'b': 0.5}),
+     ({}, {'a': 0.2, 'b': 0.5},
+      {'a': 0.2, 'b': 0.5}, {'a': 0.2, 'b': 0.5}),
+     # RDTAllocations
+     ({}, {"rdt": RDTAllocation(name='', l3='ff')},
+      {"rdt": RDTAllocation(name='', l3='ff')}, {"rdt": RDTAllocation(name='', l3='ff')}),
+     ({"rdt": RDTAllocation(name='', l3='ff')}, {},
+      {"rdt": RDTAllocation(name='', l3='ff')}, {}),
+     ({"rdt": RDTAllocation(name='', l3='ff')}, {"rdt": RDTAllocation(name='x', l3='ff')},
+      {"rdt": RDTAllocation(name='x', l3='ff')}, {"rdt": RDTAllocation(name='x', l3='ff')}),
+     ({"rdt": RDTAllocation(name='x', l3='ff')}, {"rdt": RDTAllocation(name='x', l3='dd')},
+      {"rdt": RDTAllocation(name='x', l3='dd')}, {"rdt": RDTAllocation(name='x', l3='dd')}),
+     ({"rdt": RDTAllocation(name='x', l3='dd', mb='ff')}, {"rdt": RDTAllocation(name='x', mb='ff')},
+      {"rdt": RDTAllocation(name='x', l3='dd', mb='ff')}, {}),
+    ))
+def test_calculate_task_allocations(
+        current_task_allocations, new_task_allocations,
+        expected_target_task_allocations, expected_task_allocations_changeset):
+    target_task_allocations, task_allocations_changeset = _calculate_task_allocations_changeset(
+        current_task_allocations, new_task_allocations
+    )
+    assert target_task_allocations == expected_target_task_allocations
+    assert task_allocations_changeset == expected_task_allocations_changeset
+
+
+@pytest.mark.parametrize(
+    'current_tasks_allocations,new_tasks_allocations,'
+    'expected_target_tasks_allocations,expected_tasks_allocations_changeset', (
+        ({}, {},
+         {}, {}),
+        (dict(t1={'a': 2}), {},
+         dict(t1={'a': 2}), {}),
+        (dict(t1={'a': 1}), dict(t1={'b': 2}, t2={'b': 3}),
+         dict(t1={'a': 1, 'b': 2}, t2={'b': 3}), dict(t1={'b': 2}, t2={'b': 3})),
+    ))
+def test_calculate_tasks_allocations_changeset(
+        current_tasks_allocations, new_tasks_allocations,
+        expected_target_tasks_allocations, expected_tasks_allocations_changeset
+):
+    target_tasks_allocations, tasks_allocations_changeset = _calculate_tasks_allocations_changeset(
+        current_tasks_allocations, new_tasks_allocations
+    )
+    assert target_tasks_allocations == expected_target_tasks_allocations
+    assert tasks_allocations_changeset == expected_tasks_allocations_changeset
+
+
+@pytest.mark.parametrize('hexstr,expected_bits_count', (
+        ('', 0),
+        ('1', 1),
+        ('2', 1),
+        ('3', 2),
+        ('f', 4),
+        ('f0', 4),
+        ('0f0', 4),
+        ('ff0', 8),
+        ('f1f', 9),
+        ('fffff', 20),
+))
+def test_count_enabled_bits(hexstr, expected_bits_count):
+    got_bits_count = _count_enabled_bits(hexstr)
+    assert got_bits_count == expected_bits_count
+
+
+@pytest.mark.parametrize('line,expected_domains', (
+        ('', {}),
+        ('x=2', {'x': '2'}),
+        ('x=2;y=3', {'x': '2', 'y': '3'}),
+        ('foo=bar', {'foo': 'bar'}),
+        ('mb:1=20;2=50',  {'1': '20', '2': '50'}),
+        ('mb:xxx=20mbs;2=50b', {'xxx': '20mbs', '2': '50b'}),
+        ('l3:0=20;1=30', {'1': '30', '0': '20'}),
+))
+def test_parse_schemata_file_row(line, expected_domains):
+    got_domains = _parse_schemata_file_row(line)
+    assert got_domains == expected_domains
+
+
+@pytest.mark.parametrize('invalid_line,expected_message', (
+        ('x=', 'value cannot be empty'),
+        ('x=2;x=3', 'Conflicting domain id found!'),
+        ('=2', 'domain_id cannot be empty!'),
+        ('2', 'Value separator is missing "="!'),
+        (';', 'domain cannot be empty'),
+        ('xxx', 'Value separator is missing "="!'),
+))
+def test_parse_invalid_schemata_file_domains(invalid_line, expected_message):
+    with pytest.raises(ValueError, match=expected_message):
+        _parse_schemata_file_row(invalid_line)
+
+
+def rdt_metric_func(type, value, **labels):
+    """Helper to create RDT like metric"""
+    return Metric(
+      name='allocation',
+      type=MetricType.GAUGE,
+      value=value,
+      labels=dict(allocation_type=type, **(labels or dict()))
+    )
+
+
+@pytest.mark.parametrize('rdt_allocation, expected_metrics', (
+    (RDTAllocation(), []),
+    (RDTAllocation(mb='mb:0=20'), [
+          rdt_metric_func('rdt_mb', 20, group_name='', domain_id='0')
+    ]),
+    (RDTAllocation(mb='mb:0=20;1=30'), [
+        rdt_metric_func('rdt_mb', 20, group_name='', domain_id='0'),
+        rdt_metric_func('rdt_mb', 30, group_name='', domain_id='1'),
+    ]),
+    (RDTAllocation(l3='l3:0=ff'), [
+        rdt_metric_func('rdt_l3_cache_ways', 8, group_name='', domain_id='0'),
+        rdt_metric_func('rdt_l3_mask', 255, group_name='', domain_id='0'),
+    ]),
+    (RDTAllocation(name='be', l3='l3:0=ff', mb='mb:0=20;1=30'), [
+        rdt_metric_func('rdt_l3_cache_ways', 8, group_name='be', domain_id='0'),
+        rdt_metric_func('rdt_l3_mask', 255, group_name='be', domain_id='0'),
+        rdt_metric_func('rdt_mb', 20, group_name='be', domain_id='0'),
+        rdt_metric_func('rdt_mb', 30, group_name='be', domain_id='1'),
+    ]),
+))
+def test_rdt_allocation_generate_metrics(rdt_allocation: RDTAllocation, expected_metrics):
+    got_metrics = rdt_allocation.generate_metrics()
+    assert got_metrics == expected_metrics
+
+
+@pytest.mark.parametrize('tasks_allocations,expected_metrics', (
+    ({}, []),
+    ({'some_task': {AllocationType.SHARES: 0.5}}, [
+        Metric(name='allocation', value=0.5,
+               type=MetricType.GAUGE,
+               labels={'allocation_type': 'cpu_shares', 'task_id': 'some_task'})
+    ]),
+    ({'some_task': {AllocationType.RDT: RDTAllocation(mb='mb:0=20')}}, [
+        rdt_metric_func('rdt_mb', 20, group_name='', domain_id='0', task_id='some_task')
+    ]),
+    ({'some_task': {AllocationType.SHARES: 0.5, AllocationType.RDT: RDTAllocation(mb='mb:0=20')}}, [
+        Metric(
+            name='allocation', value=0.5,
+            type=MetricType.GAUGE,
+            labels={'allocation_type': AllocationType.SHARES, 'task_id': 'some_task'}
+        ),
+        rdt_metric_func('rdt_mb', 20, group_name='', domain_id='0', task_id='some_task')
+    ]),
+    ({'some_task_a': {
+            AllocationType.SHARES: 0.5, AllocationType.RDT: RDTAllocation(mb='mb:0=30')
+        },
+      'some_task_b': {
+            AllocationType.QUOTA: 0.6,
+            AllocationType.RDT: RDTAllocation(name='b', l3='l3:0=f;1=f1'),
+        }}, [
+        Metric(
+            name='allocation', value=0.5,
+            type=MetricType.GAUGE,
+            labels={'allocation_type': AllocationType.SHARES, 'task_id': 'some_task_a'}
+        ),
+        rdt_metric_func('rdt_mb', 30, group_name='', domain_id='0', task_id='some_task_a'),
+        Metric(
+            name='allocation', value=0.6,
+            type=MetricType.GAUGE,
+            labels={'allocation_type': AllocationType.QUOTA, 'task_id': 'some_task_b'}
+        ),
+        rdt_metric_func('rdt_l3_cache_ways', 4, group_name='b',
+                        domain_id='0', task_id='some_task_b'),
+        rdt_metric_func('rdt_l3_mask', 15, group_name='b', domain_id='0', task_id='some_task_b'),
+        rdt_metric_func('rdt_l3_cache_ways', 5, group_name='b',
+                        domain_id='1', task_id='some_task_b'),
+        rdt_metric_func('rdt_l3_mask', 241, group_name='b', domain_id='1', task_id='some_task_b'),
+    ]),
+))
+def test_convert_task_allocations_to_metrics(tasks_allocations, expected_metrics):
+    metrics_got = _convert_tasks_allocations_to_metrics(tasks_allocations)
+    assert metrics_got == expected_metrics
+
+
+@pytest.mark.parametrize(
+    'current_rdt_alloaction, new_rdt_allocation,'
+    'expected_target_rdt_allocation,expected_rdt_allocation_changeset', (
+        (None, RDTAllocation(),
+         RDTAllocation(), RDTAllocation()),
+        (RDTAllocation(name=''), RDTAllocation(),  # empty group overrides existing
+         RDTAllocation(), RDTAllocation()),
+        (RDTAllocation(), RDTAllocation(l3='x'),
+         RDTAllocation(l3='x'), RDTAllocation(l3='x')),
+        (RDTAllocation(l3='x'), RDTAllocation(mb='y'),
+         RDTAllocation(l3='x', mb='y'), RDTAllocation(mb='y')),
+        (RDTAllocation(l3='x'), RDTAllocation(l3='x', mb='y'),
+         RDTAllocation(l3='x', mb='y'), RDTAllocation(mb='y')),
+        (RDTAllocation(l3='x', mb='y'), RDTAllocation(name='new', l3='x', mb='y'),
+         RDTAllocation(name='new', l3='x', mb='y'), RDTAllocation(name='new', l3='x', mb='y')),
+        (RDTAllocation(l3='x'), RDTAllocation(name='', l3='x'),
+         RDTAllocation(name='', l3='x'), RDTAllocation(name='', l3='x'))
+    )
+)
+def test_merge_rdt_allocations1(
+        current_rdt_alloaction, new_rdt_allocation,
+        expected_target_rdt_allocation, expected_rdt_allocation_changeset):
+    got_target_rdt_allocation, got_rdt_alloction_changeset = \
+        new_rdt_allocation.merge_with_current(current_rdt_alloaction)
+
+    assert got_target_rdt_allocation == expected_target_rdt_allocation
+    assert got_rdt_alloction_changeset == expected_rdt_allocation_changeset
