e73774cba738ca4f2689650eaa4dfad277298ae8
Pawel Palucki <pawel.palucki@intel.com>
2019-03-14 19:14:20 +0100

    check metrics existance

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,5 +1,21 @@
+# Copyright (c) 2019 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from unittest.mock import patch, call, mock_open
-from owca.testing import create_open_mock
+from owca.testing import create_open_mock, _is_dict_match, assert_metric
+from owca.metrics import Metric
+import pytest
 
 
 def test_create_open_mock_autocreated():
@@ -21,3 +37,42 @@ def test_create_open_mock_manually():
         open('/some/path.txt', 'w').write('bar')
         my_own_mock_open.assert_has_calls(
             [call().write('bar')])
+
+
+@pytest.mark.parametrize('got_dict, expected_subdict, expected_match', [
+    (dict(), dict(), True),  # Empty query always match
+    (dict(x=1), dict(), True),  # ditto,
+    (dict(), dict(x=1), False),  # Non empty query doesn't match empty dict,
+    (dict(x=2), dict(x=1), False),  # no when value is different
+    (dict(x=1), dict(x=1), True),  # ok for the same
+    (dict(x=1, y=2), dict(x=1), True),  # additional parameters doesn't interfere
+    (dict(x=1, y=2), dict(x=1, y=2), True),  # two params exact match
+    (dict(x=1, y=1, z=3), dict(x=1, y=2), False),  # two keys mismatch
+    (dict(x=1, y=2, z=3), dict(x=1, y=2), True),  # two vs three keys match
+])
+def test_match_keys(got_dict, expected_subdict, expected_match):
+    assert _is_dict_match(got_dict, expected_subdict) == expected_match
+
+
+@pytest.mark.parametrize(
+    'got_metrics, expected_metric_name, '
+    'expected_metric_labels, expected_metric_value, exception_message', [
+        ([Metric('foo', 2)], 'foo', None, None, None),
+        ([Metric('foo', 2)], 'foo', None, 2, None),
+        ([Metric('foo', 2)], 'foo', None, 3, 'metric value differs'),
+        ([Metric('foo', 2)], 'foo', dict(), 2, None),
+        ([Metric('foo', 2, labels=dict(a='b'))], 'foo', None, 3, 'metric value differs'),
+        ([Metric('foo', 2, labels=dict(a='b'))], 'foo', None, 2, None),
+        ([Metric('foo', 2, labels=dict(a='b'))], 'foo', dict(), 2, None),
+        ([Metric('foo', 2, labels=dict(a='b'))], 'foo', dict(a='b'), 2, None),
+        ([Metric('foo', 2, labels=dict(a='b'))], 'foo', dict(a='c'), 2, 'not found'),
+    ])
+def test_assert_metric(got_metrics, expected_metric_name,
+                       expected_metric_labels, expected_metric_value, exception_message):
+    if exception_message is not None:
+        with pytest.raises(AssertionError, match=exception_message):
+            assert_metric(got_metrics, expected_metric_name,
+                          expected_metric_labels, expected_metric_value)
+    else:
+        assert_metric(got_metrics, expected_metric_name,
+                      expected_metric_labels, expected_metric_value)
