899f35f95edb9b8e4bcbb33dda38e35ceb0caa55
Pawel Palucki <pawel.palucki@intel.com>
2019-03-25 16:38:19 +0100

    remove duplicated anomaly_metrics

--- tests/test_anomalies.py
+++ tests/test_anomalies.py
@@ -11,42 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import List, Dict
 
 import pytest
 
-from owca.detectors import convert_anomalies_to_metrics, _create_uuid_from_tasks_ids, \
-    ContendedResource
-from owca.metrics import Metric, MetricType
-from owca.nodes import TaskId
-from owca.testing import anomaly
-
-
-def anomaly_metrics(contended_task_id: TaskId, contending_task_ids: List[TaskId],
-                    contending_workload_instances: Dict[TaskId, str] = None,
-                    labels: Dict[TaskId, Dict[str, str]] = None):
-    """Helper method to create metric based on anomaly.
-    uuid is used if provided.
-    """
-    contending_workload_instances = contending_workload_instances or dict()
-    labels = labels or dict()
-    metrics = []
-    for task_id in contending_task_ids:
-        uuid = _create_uuid_from_tasks_ids(contending_task_ids + [contended_task_id])
-        metric = Metric(
-            name='anomaly', value=1,
-            labels=dict(
-                contended_task_id=contended_task_id, contending_task_id=task_id,
-                resource=ContendedResource.MEMORY_BW, uuid=uuid,
-                type='contention',
-                contending_workload_instance=contending_workload_instances[task_id],
-                workload_instance=contending_workload_instances[contended_task_id]),
-            type=MetricType.COUNTER
-        )
-        if contended_task_id in labels:
-            metric.labels.update(labels[contended_task_id])
-        metrics.append(metric)
-    return metrics
+from owca.detectors import convert_anomalies_to_metrics
+from owca.testing import anomaly, anomaly_metrics
 
 
 @pytest.mark.parametrize('anomalies,tasks_labels,expected_metrics', (
