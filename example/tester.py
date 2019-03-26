acd7fc83f63afb4ad67ca145b95308d5cf2c0063
Pawel Palucki <pawel.palucki@intel.com>
2019-03-26 14:17:16 +0100

    example tester

--- example/tester.py
+++ example/tester.py
@@ -1,8 +1,61 @@
 import argparse
-import yaml
+import logging
 import os
-import subprocess
 import signal
+import subprocess
+from typing import List
+
+from owca.config import register
+from attr import dataclass
+
+from owca.allocators import Allocator, TasksAllocations
+from owca.config import load_config
+from owca.detectors import TasksMeasurements, TasksResources, TasksLabels, Anomaly
+from owca.metrics import Metric
+from owca.nodes import Node, Task
+from owca.platforms import Platform
+from owca.storage import Storage
+
+log = logging.getLogger(__name__)
+
+
+@register
+@dataclass
+class Tester(Node, Allocator, Storage):
+    config: str
+
+    def __post_init__(self):
+        #self.config
+        self.config_data = load_config(self.config)
+
+        #
+        self.metrics = []
+
+    def get_tasks(self) -> List[Task]:
+
+        # jesli to jest second run:
+
+
+
+        # bazujesz na wartosci z yamla z danego testcase i pola tasks
+
+        # towrzyorzy co potrzebne w systemie
+
+
+        return [Task()]
+
+    def allocate(self, platform: Platform, tasks_measurements: TasksMeasurements,
+                 tasks_resources: TasksResources, tasks_labels: TasksLabels,
+                 tasks_allocations: TasksAllocations) -> (
+            TasksAllocations, List[Anomaly], List[Metric]):
+        ...
+        # tu kod ze static_allocatora parsujacy rule
+        # z yamka z allocation_rules
+
+    def store(self, metrics: List[Metric]) -> None:
+        self.metrics.extend(metrics)
+
+
 
 
 def _get_arguments():
@@ -56,7 +109,8 @@ def _delete_cgroup(task):
     os.system(command.format(perf_path))
 
 
-def _handle_test_case(case, prev_tasks, tasks_file_path, allocations_file_path, check_sleep, test_sleep):
+def _handle_test_case(case, prev_tasks, tasks_file_path, allocations_file_path, check_sleep,
+                      test_sleep):
     pids = []
     tasks = []
 
@@ -69,13 +123,14 @@ def _handle_test_case(case, prev_tasks, tasks_file_path, allocations_file_path, 
             pid = _create_dumb_process(task)
             pids.append(pid)
             os.kill(pid, signal.SIGKILL)
-            import IPython; IPython.embed()
+            import IPython;
+            IPython.embed()
             prev_tasks.add(task)
             tasks.append(
-                    {'name': '{}_name'.format(task),
-                        'task_id': task,
-                        'cgroup_path': '/{}'.format(task)
-                        })
+                {'name': '{}_name'.format(task),
+                 'task_id': task,
+                 'cgroup_path': '/{}'.format(task)
+                 })
         with open(tasks_file_path, 'w') as f:
             f.write(yaml.dump(tasks))
         time.sleep(test_sleep)
@@ -94,7 +149,8 @@ def main():
     prev_tasks = set()
 
     for case in config['tests']:
-        _handle_test_case(config['tests'][case], prev_tasks, tasks_file_path, allocations_file_path, check_sleep, test_sleep)
+        _handle_test_case(config['tests'][case], prev_tasks, tasks_file_path, allocations_file_path,
+                          check_sleep, test_sleep)
 
 
 if __name__ == '__main__':
