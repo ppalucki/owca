f0919598f05b070c93c53ef07fbdeb86ac03e9de
Creatone <pawel.szulik@intel.com>
2019-03-27 15:09:21 +0100

    [WIP] Tester

--- example/tester.py
+++ example/tester.py
@@ -1,15 +1,10 @@
-import argparse
 import logging
 import os
 import signal
-import time
-import yaml
 import subprocess
+
+from dataclasses import dataclass
 from typing import List
-
-from owca.config import register
-from attr import dataclass
-
 from owca.allocators import Allocator, TasksAllocations
 from owca.config import load_config
 from owca.detectors import TasksMeasurements, TasksResources, TasksLabels, Anomaly
@@ -24,112 +19,110 @@ CPU_PATH = '/sys/fs/cgroup/cpu/{}/tasks'
 PERF_PATH = '/sys/fs/cgroup/perf_event/{}/tasks'
 
 
-@register
 @dataclass
 class Tester(Node, Allocator, Storage):
     config: str
 
     def __post_init__(self):
         self.config_data = load_config(self.config)
-
+        self.test_number = 0
         self.metrics = []
+        self.pids = []
+        self.tasks = List[Task]
 
     def get_tasks(self) -> List[Task]:
-        # jesli to jest second run:
+        test_case = self.config_data['tests'][self.test_number]
 
+        if self.test_number > 0:
+            # delete processes before
+            # delete cgroups
+            for check in test_case['checks']:
+                # do checks
+                pass
 
+            for pid in self.pids:
+                _kill_dumb_process(pid)
 
-        # bazujesz na wartosci z yamla z danego testcase i pola tasks
+            for task in self.tasks:
+                _delete_cgroup(task.cgroup_path)
 
-        # towrzyorzy co potrzebne w systemie
+            pass
 
+        for task_name in test_case['tasks']:
+            name, task_id, cgroup_path = _parse_task_name(task_name)
+            task = Task(name=name, task_id=task_id, cgroup_path=cgroup_path)
 
-        return [Task()]
+            _create_cgroup(cgroup_path)
+
+            pid = _create_dumb_process(cgroup_path)
+            self.pids.append(pid)
+
+            self.tasks.append(task)
+
+        return self.tasks
 
     def allocate(self, platform: Platform, tasks_measurements: TasksMeasurements,
                  tasks_resources: TasksResources, tasks_labels: TasksLabels,
                  tasks_allocations: TasksAllocations) -> (
             TasksAllocations, List[Anomaly], List[Metric]):
-        ...
+        pass
         # tu kod ze static_allocatora parsujacy rule
         # z yamka z allocation_rules
 
     def store(self, metrics: List[Metric]) -> None:
         self.metrics.extend(metrics)
 
-    def _create_dumb_process(self, task):
-        command = ['sleep', 'inf']
-        p = subprocess.Popen(command)
-        cpu_path, perf_path = self._get_cgroup_path(task)
-        with open(cpu_path, 'a') as f:
-            f.write(str(p.pid))
-        with open(perf_path, 'a') as f:
-            f.write(str(p.pid))
 
-        return p.pid
-
-    def _kill_dumb_process(self, pid):
-        os.kill(pid, signal.SIGKILL)
-
-    def _get_cgroup_path(self, task):
-        return CPU_PATH.format(task), PERF_PATH.format(task)
-
-    def _create_cgroup(self, task):
-        cpu_path, perf_path = self._get_cgroup_path(task)
-        try:
-            os.makedirs(cpu_path.format(task))
-        except FileExistsError:
-            print('{} already in cpu cgroup'.format(task))
-
-        try:
-            os.makedirs(perf_path.format(task))
-        except FileExistsError:
-            print('{} already in perf_event cpgroup'.format(task))
-
-    def _delete_cgroup(self, task):
-        cpu_path, perf_path = self._get_cgroup_path(task)
-        command = 'sudo find {0} -depth -type d -print -exec rmdir {{}} \\;'
-        import IPython; IPython.embed()
-        try:
-            os.system(command.format(cpu_path))
-        except FileNotFoundError:
-            print('{} not found in cpu cgroup'.format(task))
-
-        try:
-            os.system(command.format(perf_path))
-        except FileNotFoundError:
-            print('{} not found in perf_event cgroup'.format(task))
+def _parse_task_name(task):
+    name = task.split('/')[-1]
+    return name, name, task
 
 
-def _handle_test_case(case, prev_tasks, tasks_file_path, allocations_file_path, check_sleep, test_sleep):
-    pids = set()
+def _create_dumb_process(task):
+    command = ['sleep', 'inf']
+    p = subprocess.Popen(command)
+    cpu_path, perf_path = _get_cgroup_full_path(task)
+    with open(cpu_path, 'a') as f:
+        f.write(str(p.pid))
+    with open(perf_path, 'a') as f:
+        f.write(str(p.pid))
 
-    for task in case['tasks']:
-        _create_cgroup(task)
-        pid = _create_dumb_process(task)
-        pids.add(pid)
-        prev_tasks.add(task)
-        time.sleep(int(test_sleep))
-        _kill_dumb_process(pid)
-        pids.remove(pid)
-        _delete_cgroup(task)
+    return p.pid
 
 
-def main():
-    tester_config = _get_arguments()
-    config = _parse_config(tester_config)
-
-    tasks_file_path = config['tasks_filename']
-    allocations_file_path = config['allocations_filename']
-    check_sleep = config['check_sleep']
-    test_sleep = config['test_sleep']
-
-    prev_tasks = set()
-
-    for case in config['tests']:
-        _handle_test_case(config['tests'][case], prev_tasks, tasks_file_path, allocations_file_path,
-                          check_sleep, test_sleep)
+def _kill_dumb_process(pid):
+    os.kill(pid, signal.SIGKILL)
 
 
-if __name__ == '__main__':
-    main()
+def _get_cgroup_full_path(cgroup):
+    return CPU_PATH.format(cgroup), PERF_PATH.format(cgroup)
+
+
+def _create_cgroup(cgroup_path):
+    cpu_path, perf_path = _get_cgroup_full_path(cgroup_path)
+
+    try:
+        os.makedirs(cpu_path.format(cgroup_path))
+    except FileExistsError:
+        log.warning('cpu cgroup "{}" already exists'.format(cgroup_path))
+
+    try:
+        os.makedirs(perf_path.format(cgroup_path))
+    except FileExistsError:
+        log.warning('perf_event cgroup "{}" already exists'.format(cgroup_path))
+
+
+# TODO: Refactor
+def _delete_cgroup(cgroup_path):
+    cpu_path, perf_path = _get_cgroup_full_path(cgroup_path)
+    command = 'sudo find {0} -depth -type d -print -exec rmdir {{}} \\;'
+
+    try:
+        os.system(command.format(cpu_path))
+    except FileNotFoundError:
+        log.warning('cpu cgroup "{}" not found'.format(cgroup_path))
+
+    try:
+        os.system(command.format(perf_path))
+    except FileNotFoundError:
+        log.warning('perf_event cgroup "{}" not found'.format(cgroup_path))
