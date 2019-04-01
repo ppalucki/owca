2100a20e99b0ffcd557e4a14b6e791fb8b4bf17a
Pawel Palucki <pawel.palucki@intel.com>
2019-04-01 19:15:53 +0200

    fix flakes

--- owca/runners/allocation.py
+++ owca/runners/allocation.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 import logging
 import time
-from typing import Dict, Callable, Any, Optional
+from typing import Dict, Callable, Any
 
 from owca import nodes, storage, platforms
 from owca import resctrl
