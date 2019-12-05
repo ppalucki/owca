f294235599755bd6a9c3dbbd21de9fcff5272f8b
Pawel Palucki <pawel.palucki@intel.com>
2019-12-05 14:08:50 +0100

    storea allocations in Kafka as well

--- wca/storage.py
+++ wca/storage.py
@@ -97,7 +97,6 @@ class LogStorage(Storage):
             self._output = sys.stderr
 
     def store(self, metrics):
-        log.debug('Storing %d metrics to %s.', len(metrics), self.output_filename or 'None(stderr)')
         log.log(logger.TRACE, 'Dump of metrics: %r', metrics)
 
         is_convertable, error_message = is_convertable_to_prometheus_exposition_format(metrics)
@@ -114,6 +113,8 @@ class LogStorage(Storage):
                 timestamp = None
             msg = convert_to_prometheus_exposition_format(metrics, timestamp, self.filter_labels)
             log.log(logger.TRACE, 'Dump of metrics (text format): %r', msg)
+            log.debug('LogStorage: Storing %d metrics to %s (%s).', len(metrics), len(msg),
+                      self.output_filename or 'None(stderr)')
             if self.overwrite:
                 p = pathlib.Path(self.output_filename)
                 p_tmp = p.with_suffix('.tmp')
@@ -471,7 +472,7 @@ class KafkaStorage(Storage):
                     "Message has failed to be writen to kafka. API error message: {}.".format(
                         error_from_callback__original_ref))
 
-            log.debug('message size=%i with timestamp=%s stored in kafka topic=%r',
+            log.debug('KafkaStorage: Message size=%i with timestamp=%s stored in kafka topic=%r',
                       len(msg), timestamp, self.topic)
 
         return  # the message has been send to kafka
