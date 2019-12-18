6dd5a32c8258b2081f53cde98c086eb09a8d4695
Damian Darczuk <damian.darczuk@intel.com>
2019-12-18 13:12:08 +0100

    Remove watch_scripe.sh

--- examples/kubernetes/workloads/specjbb/watch_scripe.sh
+++
@@ -1,3 +0,0 @@
-while true; do kubectl -n ddarczuk logs specjbb-0 -c controler -f; sleep 2; done
-while true; do kubectl -n ddarczuk logs specjbb-0 -c backend -f; sleep 2; done
-while true; do kubectl -n ddarczuk logs specjbb-0 -c injector -f; sleep 2; done
