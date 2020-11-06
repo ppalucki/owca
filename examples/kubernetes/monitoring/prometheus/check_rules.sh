c5378e3f0e08f9fc2ea88e538a57ccec8b414ad6
Pawel Palucki <pawel.palucki@intel.com>
2020-11-06 20:38:31 +0100

    almost ready app_wss_v2

--- examples/kubernetes/monitoring/prometheus/check_rules.sh
+++ examples/kubernetes/monitoring/prometheus/check_rules.sh
@@ -1,6 +1,27 @@
 #!/bin/zsh
+#
+if  ! type zsh >/dev/null; then
+    echo "ERROR zsh (zsh for yaml) is required!"
+    exit 1
+fi
+
+if  ! type promtool >/dev/null; then
+    echo "ERROR promtool is required!"
+    exit 1
+fi
+
+if  ! type yq >/dev/null; then
+    echo "ERROR yq (jq for yaml) is required!"
+    exit 1
+fi
+
 for f in prometheusrules*.yaml **/prometheusrules*.yaml; 
 do 
     echo $f
     promtool check rules <(cat $f | yq .spec)
+    
+    if [ $? != 0 ]; then
+        echo ERROR when checking above rules $f
+        exit 1
+    fi
 done
